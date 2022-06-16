6daf9da09ca4512753eedbcd6b12051bd7b90dcf
Ayaz Mehmood <77320646+ayazmhmd@users.noreply.github.com>
2022-06-17 02:38:20 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import tensorflow
+import numpy as np
+from flask import Flask, render_template, request
+import prediction
+result =	{
+  0: "Explosion",
+  1: "Fighting",
+  2:  "Burgulary",
+}
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+app=Flask(__name__)
+@app.route('/')
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST': 
+	 Vid = request.files['my_video']
+
+	 vid_path = Vid.filename	
+	 Vid.save(vid_path)
+	 imges=prediction.video_capture(vid_path)
+	 img=np.asarray(imges)	
+	 ret=prediction.check(img)	
+	 val=result[ret]
+	 print(val)
+	 try:
+		 os.remove(vid_path)
+	 except:
+		 pass
+	 return render_template("index.html", Pred = ret,cat=val, vid_name=Vid.filename)
+if __name__=='__main__':
+    app.run(debug=True,use_reloader=False)
---
+++ prediction.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+from tensorflow.keras.models import load_model
+print(cv2.__version__)
+def video_capture(vid_path):
+    # video_name=os.path.splitext(video_file)[0]
+    # vid_path=os.path.join(path1,video_file)
+    vid_capture = cv2.VideoCapture(vid_path) 
+    if (vid_capture.isOpened() == False):
+      print("Error opening the video file")
+      return None
+    else:
+      d=0
+      counter=0
+      cat_images=[]
+    while(vid_capture.isOpened()):
+      ret, frame = vid_capture.read()
+      if ret == True:
+          d=d+1
+          if d <16:
+            continue
+          else:
+            counter=counter+1
+            d=0
+            frame_grey=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            datu=np.array(frame_grey)
+            normu_dat=datu/255
+            cat_images.append(normu_dat)
+            if counter==60:
+              break
+      else:
+        if counter<60:
+          while counter<60:
+            cat_images.append(normu_dat)
+            counter=counter+1
+        break
+    vid_capture.release()
+    cv2.destroyAllWindows()
+    return cat_images
+
+
+def check(images):
+    new_model = tf.keras.models.load_model('3dcnn_weight.h5',compile = True)
+    test_img=np.expand_dims(images, axis=0)
+    x=new_model.predict(test_img)
+    b=np.argmax(x)
+    print(b)
+    return b
+
